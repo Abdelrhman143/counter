4ff7fd47e780339458688de0bd729c219f04fc26
Abdelrhman143 <aa632481@gmail.com>
2025-05-01 16:08:24 +0300

    adding full logic to the app

--- src/App.jsx
+++ src/App.jsx
@@ -12,28 +12,71 @@ function App() {
 function Counter() {
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
+  // update date using useState
+  // const [date, setDate] = useState(new Date());
+
+  // updating date without using useState
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  const nowDate = new Date().toDateString();
 
   function handleStepAdding() {
-    setStep((s) => step + 1);
+    setStep((s) => s + 1);
   }
 
   function handleStepMinus() {
-    setStep((s) => step - 1);
+    setStep((s) => s - 1);
   }
 
+  function handlecountAdding() {
+    setCount((c) => c + step);
+    // setDate((CurrDate) => {
+    //   const newDate = new Date(CurrDate);
+    //   newDate.setDate(newDate.getDate() + step);
+    //   return newDate;
+    // });
+  }
+
+  function handlecountMinus() {
+    setCount((c) => c - step);
+    // setDate((CurrDate) => {
+    //   const newDate = new Date(CurrDate);
+    //   newDate.setDate(newDate.getDate() - step);
+    //   return newDate;
+    // });
+  }
+
+  let message = nowDate;
+  if (date.toDateString() < nowDate) {
+    message = `${Math.abs(count)} Days from Today is ${date}`;
+  } else if (date.toDateString() > nowDate) {
+    message = `${Math.abs(count)} days ago was ${date}`;
+  }
   return (
     <div>
       <div>
-        <button className="minus">-</button>
+        <button onClick={handleStepMinus} className="minus">
+          -
+        </button>
         <span className="stpes"> step : {step}</span>
-        <button className="plus">+</button>
+        <button onClick={handleStepAdding} className="plus">
+          +
+        </button>
       </div>
       <div>
-        <button className="minus">-</button>
+        <button onClick={handlecountMinus} className="minus">
+          -
+        </button>
         <span className="stpes"> Count : {count}</span>
-        <button className="plus">+</button>
+        <button onClick={handlecountAdding} className="plus">
+          +
+        </button>
       </div>
-      <div>Today is Mon Jun 21 2027</div>
+      <div>
+        {" "}
+        <span>{message}</span>
+      </div>
     </div>
   );
 }
